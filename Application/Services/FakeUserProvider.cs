c84b612cab318b46a0ece0266321dd157a8fd152
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-03-22 02:14:10 +0300

    implemented fake UserProvider

---
+++ Application/Services/FakeUserProvider.cs
@@ -0,0 +1,13 @@
+namespace Application.Services;
+
+public class FakeUserProvider : IUserProvider
+{
+    // todo: return authenticated user
+    private readonly IUserRepository _repository;
+
+    public FakeUserProvider(IUserRepository repository)
+        => _repository = repository;
+
+    public async Task<User> GetUser()
+        => await _repository.GetFirstUser();
+}
