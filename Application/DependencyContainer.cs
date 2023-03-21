82d091371f884f269cc846db8810eecd295471d4
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-03-22 02:14:36 +0300

    service registrations and packages

---
+++ Application/DependencyContainer.cs
@@ -0,0 +1,22 @@
+namespace Application;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection RegisterApplication(this IServiceCollection services)
+    {
+        services
+            .AddMapper()
+            .AddFluentValidationValidators()
+            .AddScoped<IRatingService, RatingService>()
+            .AddScoped<IUserProvider, FakeUserProvider>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddMapper(this IServiceCollection services)
+        => services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    private static IServiceCollection AddFluentValidationValidators(this IServiceCollection services)
+        => services
+            .AddValidatorsFromAssemblyContaining<GetAverageRatingOfProviderRequestModelValidator>();
+}
---
+++ Application/GlobalUsings.cs
@@ -0,0 +1,12 @@
+global using Application.Abstract;
+global using Application.Services;
+global using Application.Validators.RatingApi;
+global using AutoMapper;
+global using Domain.Dtos;
+global using Domain.Entities;
+global using Domain.Notifications;
+global using Domain.Requests.Ratings;
+global using Domain.Responses.Ratings;
+global using FluentValidation;
+global using Microsoft.Extensions.DependencyInjection;
+global using Persistence.Abstract;
