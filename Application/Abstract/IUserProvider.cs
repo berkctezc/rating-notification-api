c4d7cc203f10975f17f86b92df9b973cea81181f
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-03-23 18:18:29 +0300

    added documentation for some services

--- Application/Abstract/IUserProvider.cs
+++ Application/Abstract/IUserProvider.cs
@@ -2,5 +2,9 @@ namespace Application.Abstract;
 
 public interface IUserProvider
 {
+    /// <summary>
+    /// a basic function to get a user
+    /// </summary>
+    /// <returns></returns>
     Task<User> GetUser();
 }
